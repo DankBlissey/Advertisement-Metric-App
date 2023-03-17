9121d92649425ede4820aa54dfade4cc34e7b1aa
tstokes <tgs1g21@soton.ac.uk>
2023-03-17 17:53:08 +0000

    the controller doesn't store any filters anymore, instead it stores a FilterWindow(subjectToChange) so that it can get the list of Filter objects when the granularity is updated.

--- app/src/main/java/uk/ac/soton/adDashboard/controller/Controller.java
+++ app/src/main/java/uk/ac/soton/adDashboard/controller/Controller.java
@@ -3,22 +3,29 @@ package uk.ac.soton.adDashboard.controller;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.util.Pair;
+import uk.ac.soton.adDashboard.Interfaces.FilterWindow;
 import uk.ac.soton.adDashboard.Interfaces.GraphFeatures;
+import uk.ac.soton.adDashboard.components.FilterSet;
 import uk.ac.soton.adDashboard.enums.Granularity;
 import uk.ac.soton.adDashboard.enums.Stat;
 import uk.ac.soton.adDashboard.filter.Filter;
 import uk.ac.soton.adDashboard.records.DataSet;
 
+
+/**
+ * The overarching Controller of the system.
+ */
 public class Controller {
 
   private GraphFeatures graph;
   private DataSet model;
 
-  private Filter filter;
+  private FilterWindow filterWindow;
 
   private Stat statType;
 
   private Granularity granularity = Granularity.DAY;
+
 
   public GraphFeatures getGraph() {
     return graph;
@@ -37,9 +44,7 @@ public class Controller {
     this.model = model;
   }
 
-  public Filter getFilters() {
-    return filter;
-  }
+
 
   public Stat getStatType() {
     return statType;
@@ -49,18 +54,39 @@ public class Controller {
     this.statType = statType;
   }
 
-  public void setFilters(Filter filter) {
-    this.filter= filter;
+  public void setFilterSet(FilterWindow filter) {
+    this.filterWindow= filter;
   }
+
+  public FilterWindow getFilterSet() {
+    return filterWindow;
+  }
+
 
   public Granularity getGranularity() {
     return granularity;
   }
 
+  /**
+   * Sets the granularity of the graph and points. It triggers a recalculation of all points.
+   * @param granularity The new Granularity.
+   */
   public void setGranularity(Granularity granularity) {
     this.granularity = granularity;
+    rePlot();
   }
 
+  private void rePlot() {
+    List<Filter> filters = filterWindow.getFilters();
+    for (Filter filter : filters) {
+      filterUpdated(filter);
+    }
+  }
+
+  /**
+   * Called to update the line for a Filter which has changed.
+   * @param filter The Filter object which changed.
+   */
   public void filterUpdated(Filter filter) {
 
     model.setFilter(filter);
@@ -70,6 +96,10 @@ public class Controller {
     graph.plot(filter.getId(),points);
   }
 
+  /**
+   * Deletes the line for a given Filter.
+   * @param filter The Filter to delete from the graph.
+   */
   public void deleteLine(Filter filter) {
     graph.delete(filter.getId());
   }
