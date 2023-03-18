41d2c89e35279444d21e12e2905174995a692243
Thomas <th2g20@soton.ac.uk>
2023-03-18 17:12:19 +0000

    Implemented GraphFeatures interface into Graph.java so that when the controller calls Plot and delete the line is deleted.

--- app/src/main/java/uk/ac/soton/adDashboard/views/Graph.java
+++ app/src/main/java/uk/ac/soton/adDashboard/views/Graph.java
@@ -1,25 +1,26 @@
 package uk.ac.soton.adDashboard.views;
 
 import javafx.geometry.Side;
-import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.paint.Color;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import javafx.util.Pair;
+import java.util.stream.Collectors;
 
-public class Graph {
+import javafx.util.Pair;
+import uk.ac.soton.adDashboard.Interfaces.GraphFeatures;
+
+public class Graph implements GraphFeatures {
 
     protected LineChart<Number, Number> chart;
     protected NumberAxis xAxis;
     protected NumberAxis yAxis;
-    protected List<XYChart.Series<Number, Number>> dataSeriesList;
+    protected HashMap<Integer, XYChart.Series<Number, Number>> series;
 
     public Graph() {
-        this.dataSeriesList = new ArrayList<>();
         this.chart = createChart();
+        this.series = new HashMap<>();
     }
 
     public LineChart<Number, Number> createChart() {
@@ -49,95 +50,55 @@ public class Graph {
         return chart;
     }
 
-    public LineChart<Number, Number> getChart() {
-        return this.chart;
-    }
-
     // Used to add all points to the Series
-    public void addDataPoints(List<Pair<Integer, Double>> data, XYChart.Series<Number, Number> series) {
+    private void addDataPoints(List<Pair<Number, Number>> data, XYChart.Series<Number, Number> series) {
         // Iterate through the data and add each point to the series.
-        for (Pair<Integer, Double> point : data) {
+        for (Pair<Number, Number> point : data) {
             Number xValue = point.getKey();
             Number yValue = point.getValue();
             series.getData().add(new XYChart.Data<>(xValue, yValue));
         }
     }
 
-    //Gets the Series by Index
-    public XYChart.Series<Number, Number> getSeriesByIndex(int index) {
-        if (index < 0 || index >= dataSeriesList.size()) {
-            throw new IndexOutOfBoundsException("Invalid series index: " + index);
-        }
-        return dataSeriesList.get(index);
-    }
-
-    //Used to change the points of a Series by Index in the ArrayList
-    public void resetIndexSeries(List<Pair<Integer, Double>> data, int index){
-        this.dataSeriesList.add(index,createNewSeries(data));
-    }
-
-    //Creates a new Series
-    public XYChart.Series<Number, Number> createNewSeries(List<Pair<Integer, Double>> data) {
+    // Creates a new Series
+    private XYChart.Series<Number, Number> createNewSeries(List<Pair<Integer, Double>> data) {
         // Creates a new Series
         XYChart.Series<Number, Number> newSeries = new XYChart.Series<>();
 
         // Gets the points for the series
-        addDataPoints(data, newSeries);
+        List<Pair<Number, Number>> points = data.stream()
+                .map(pair -> new Pair<Number, Number>(pair.getKey(), pair.getValue()))
+                .collect(Collectors.toList());
+        addDataPoints(points, newSeries);
 
-        //returns the new Series
+        // Returns the new Series
         return newSeries;
     }
 
-    //creates a new Series and adds it to the Series Lists and the Chart
-    public void addNewSeries(ArrayList<Pair<Integer, Double>> data) {
-        // Creates a new Series
-        XYChart.Series<Number, Number> newSeries = createNewSeries(data);
 
-        // Adds the new series to the list
-        this.dataSeriesList.add(newSeries);
+    // Adds data to an existing series in the chart
+    @Override
+    public void plot(int id, List<Pair<Integer, Double>> values) {
+        // Gets the existing series for the specified ID
+        XYChart.Series<Number, Number> existingSeries = series.get(id);
 
-        // Adds the new series to the chart
-        this.chart.getData().add(newSeries);
-    }
-
-    public void changeLineColor(Color newColor, int index) {
-        // Find the Node representing the line for the series
-        Node lineNode = getSeriesByIndex(index).getNode().lookup(".chart-series-line");
-
-        // Set the stroke color of the line
-        lineNode.setStyle("-fx-stroke: " + toRGBCode(newColor) + ";");
-    }
-
-    private String toRGBCode(Color color) {
-        return String.format("#%02X%02X%02X",
-                (int) (color.getRed() * 255),
-                (int) (color.getGreen() * 255),
-                (int) (color.getBlue() * 255));
-    }
-
-    public void deleteSeriesByIndex(int index) {
-        // Check if the index is valid
-        if (index < 0 || index >= dataSeriesList.size()) {
-            throw new IndexOutOfBoundsException("Invalid series index: " + index);
-        }
-
-        // Remove the series from the data series list and the chart
-        XYChart.Series<Number, Number> seriesToRemove = dataSeriesList.remove(index);
-        chart.getData().remove(seriesToRemove);
-
-        // Update the series index for all the remaining series
-        for (int i = index; i < dataSeriesList.size(); i++) {
-            XYChart.Series<Number, Number> series = dataSeriesList.get(i);
-            series.setName("Series " + i);  // Update the series name to reflect the new index
+        if (existingSeries == null) {
+            // If the series doesn't exist, create a new series and add it to the chart
+            existingSeries = createNewSeries(values);
+            series.put(id, existingSeries);
+            chart.getData().add(existingSeries);
+        } else {
+            // If the series already exists, add new data points to the existing series
+            for (Pair<Integer, Double> value : values) {
+                existingSeries.getData().add(new XYChart.Data<>(value.getKey(), value.getValue()));
+            }
         }
     }
 
-    public void deleteAllSeries() {
-        // Remove all series from the chart
-        chart.getData().clear();
-
-        // Clear the dataSeriesList
-        dataSeriesList.clear();
+    //removes line from the chart then removes it from the HashMap
+    @Override
+    public void delete(int id) {
+        this.chart.getData().remove(series.get(id));
+        series.remove(id);
     }
-
 }
