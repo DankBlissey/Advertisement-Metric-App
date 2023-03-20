bda69a6f5e885cea447be5b349f0962fc32ab246
Thomas <th2g20@soton.ac.uk>
2023-03-20 15:48:59 +0000

    added names for the lines in the key of the chart.

--- app/src/main/java/uk/ac/soton/adDashboard/views/Graph.java
+++ app/src/main/java/uk/ac/soton/adDashboard/views/Graph.java
@@ -64,9 +64,13 @@ public class Graph implements GraphFeatures {
     }
 
     //Creates a new Series
-    private XYChart.Series<Number, Number> createNewSeries(List<Pair<Integer, Double>> data) {
+    private XYChart.Series<Number, Number> createNewSeries(int id, List<Pair<Integer, Double>> data) {
         //Creates a new Series
         XYChart.Series<Number, Number> newSeries = new XYChart.Series<>();
+
+        //Sets key/Legend of the line in the graph
+        String name = "Filter Set " + (id + 1);
+        newSeries.setName(name);
 
         //Gets the points for the series
         List<Pair<Number, Number>> points = data.stream()
@@ -87,7 +91,7 @@ public class Graph implements GraphFeatures {
 
         if (existingSeries == null) {
             // If the series doesn't exist, create a new series and add it to the chart
-            existingSeries = createNewSeries(values);
+            existingSeries = createNewSeries(id, values);
             series.put(id, existingSeries);
             chart.getData().add(existingSeries);
         } else {
